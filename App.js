71ba6b78fdfd71a03aee663a1d032cd9829b2ef2
Gokul <xpand.developer@gmail.com>
2023-10-13 13:23:01 +0530

    Laying the foundation

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-
-// Hello World from React!!  in h1
 
 /* 
 <div id="parent">
@@ -21,6 +19,54 @@ const headingReact = React.createElement(
   ])
 );
 
+const Title = () => <h3 id="title-text">Welcome to namaste react repo</h3>;
+
+// Create a header component which have title component inside it
+
+const Header = () => {
+  let name = "Gokul";
+  return (
+    <div>
+      <h1>Hello {name}</h1>
+      {/* Component composition */}
+      <Title />
+    </div>
+  );
+};
+
+// Create header with logo, input, user icon
+
+const SearchBox = () => {
+  const [name, setName] = useState("");
+  return (
+    <input
+      type="text"
+      className="search"
+      name="Search"
+      placeholder="Search"
+      value={name}
+      onChange={(event) => {
+        console.log(event.target.value);
+        setName(event.target.value);
+      }}
+    />
+  );
+};
+
+const AdvanceHeader = () => {
+  return (
+    <div className="header">
+      <img
+        className="logo"
+        src="http://www.shadowsphotography.co/wp-content/uploads/2017/12/photography-01-800x400.jpg"
+      />
+      {/* Component composition */}
+      <SearchBox />
+      {10 + 20}
+    </div>
+  );
+};
+
 const rootReact = ReactDOM.createRoot(document.getElementById("root"));
 
-rootReact.render(headingReact);
+rootReact.render(<AdvanceHeader />);
--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # Advance-concepts
-My coding  practices and learning advance concepts.
+
+My coding practices and learning advance concepts.
+
+# Parcel:
+
+    1) Dev build
+    2) Local server
+    3) Hot module replacement
+    4) Tree shaking
+    5) Good dev experience -  Error handling
+    6) Image optimisation
+    7) Caching previous builds for faster performance
+
+    ^ - caret - updates the packages to minor versions
+    ~ - tilde - updates the packages to major versions
+
+# JSX
+
+    1) Markup + Logic
+    2) Xml or html like syntax which also holds JS functionality
+    3) attribute name should be in camelCase
+    4) JSX -> ReactElement -> Object -> HTML elements (while render)
+    5) JS operations are performed inside the curly braces
+
+# Parcel + Babel (transpiler)
+
+    - Parcel bundler have babel which is a transpiler which converts JSX to JS which browser can undrestand.
+
+# Components
+
+    1) Class based components
+    2) Functional Components - js function which return some peice of jsx from it.
+
+    RULES: Should start with Capital letter.
+           root.render(<Component />)
+
+# Component Composition
+
+    Rendering one component inside the other component.
